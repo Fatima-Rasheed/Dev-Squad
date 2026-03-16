15e4ed0dbf7b1a01b0e95ee006c935e8ace7ec6d
Fatima Rasheed <fatimarasheedch@gmail.com>
2026-03-16 03:43:05 -0700

    add

--- week 3/hackathon/tea-ecommerce/frontend/src/api/products.js
+++
@@ -1,7 +0,0 @@
-import API from './axios';
-
-export const getProducts = (params) => API.get('/products', { params });
-export const getProductById = (id) => API.get(`/products/${id}`);
-export const createProduct = (data) => API.post('/products', data);
-export const updateProduct = (id, data) => API.put(`/products/${id}`, data);
-export const deleteProduct = (id) => API.delete(`/products/${id}`);
