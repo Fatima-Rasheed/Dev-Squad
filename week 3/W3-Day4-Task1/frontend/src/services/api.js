9650c76670ca3a5713ae6304f4b9d536d564c855
Fatima Rasheed <fatimarasheedch@gmail.com>
2026-03-25 04:04:38 -0700

    final

--- week 3/W3-Day4-Task1/frontend/src/services/api.js
+++ week 3/W3-Day4-Task1/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/", // ✅ env variable
+  baseURL: import.meta.env.VITE_API_URL || " https://week3-day4backend.vercel.app", // ✅ env variable
  withCredentials: true, 
 });
 
